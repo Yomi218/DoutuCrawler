03a4c02f7d2c76b465df0354af225958e899b31f
卜俊豪 <15755337832@163.com>
2020-07-23 12:07:43 +0800

    添加项目文件。

---
+++ DoutuCrawler/Program.cs
@@ -0,0 +1,46 @@
+﻿using DoutuCrawler.Helper;
+using DoutuCrawler.Models;
+using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoutuCrawler
+{
+    class Program
+    {
+        static DoutuEntities doutuEntities = new DoutuEntities();
+        static void Main(string[] args)
+        {
+            for(var q = 0; q < 100; q++)
+            {
+                string html = HttpHelper.DownloadUrl(@"https://www.52doutu.cn/rand/");
+                HtmlDocument document = new HtmlDocument();
+                document.LoadHtml(html);
+
+                List<HtmlNode> htmlNodes = document.DocumentNode.SelectNodes("//*[@id=\"container\"]/div[1]/div").ToList();
+                var count = htmlNodes.Count;
+
+                for (var i = 0; i < count; i++)
+                {
+                    var path = "//*[@id=\"container\"]/div[1]/div[" + (i + 1) + "]/a/div/img";
+                    var node = htmlNodes[i].SelectSingleNode(path);
+                    var alt = node.Attributes["alt"].Value;
+                    var dataOriginal = node.Attributes["data-original"].Value;
+                    var dataBackup = htmlNodes[i].SelectSingleNode(path).Attributes["data-backup"].Value;
+
+                    doutuEntities.Set<Info>().Add(new Info()
+                    {
+                        Name = alt,
+                        Url = dataOriginal,
+                        UrlBackup = dataBackup
+                    });
+                    Console.WriteLine("(" + q + "-" + i + ") " + DateTime.Now + "：" + alt);
+                }
+            }
+            doutuEntities.SaveChanges();
+        }
+    }
+}
---
+++ Show/Controllers/InfoController.cs
@@ -0,0 +1,21 @@
+﻿using DoutuCrawler.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Show.Controllers
+{
+    public class InfoController : Controller
+    {
+        // GET: Show
+        public ActionResult Index()
+        {
+            DoutuEntities entity = new DoutuEntities();
+            var list = entity.Set<Info>().ToList();
+            ViewBag.InfoList = list;
+            return View();
+        }
+    }
+}
